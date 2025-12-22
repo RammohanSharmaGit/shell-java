92472252ad0de031fb36cddc30127475131f70fb
rammohansharma <rammohansharma.tech@gmail.com>
2025-12-22 19:10:03 +0530

    invalid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        System.out.print("invalid");
     }
 }
