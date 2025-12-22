b909e52fa7d3d97e6f28678288ec72c0f586b9ed
rammohansharma <rammohansharma.tech@gmail.com>
2025-12-22 19:32:29 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,10 +6,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         while(true) {
             System.out.print("$ ");
-            String command = sc.next();
+            String command = sc.nextLine();
+            String [] commandsParts = command.split(" ",2);
+
             if (command.equalsIgnoreCase("exit"))
                 break;
-            System.out.printf("%s: command not found\n", command);
+            switch(commandsParts[0]) {
+                case "echo":
+                    System.out.println(commandsParts[1]);
+                    break;
+                default:
+                    System.out.printf("%s: command not found\n", command);
+            }
+
+
         }
     }
 }
